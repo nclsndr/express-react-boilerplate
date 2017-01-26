7db68e2e01f635002fb0a1c75cbd745947a3182b
nclsndr <nclsndr1@gmail.com>
2017-01-26 19:09:52 +0100

    Home container test example

---
+++ test/src/containers/home.test.js
@@ -0,0 +1,27 @@
+/* ------------------------------------------
+ * Home container test
+ *------------------------------------------- */
+import React from 'react';
+import jsdom from 'mocha-jsdom'
+import { mount } from 'enzyme'
+import { Provider } from 'react-redux'
+import { expect } from 'chai'
+
+import configureStore from '../../../src/store'
+import Home from '../../../src/containers/home'
+
+describe('<Home />', () => {
+  jsdom()
+  let wrapper
+  before('Render component', () => {
+    const store = configureStore({})
+    wrapper = mount(
+      <Provider store={store}>
+        <Home />
+      </Provider>
+    )
+  })
+  it('Component should have .home_page class', function() {
+    expect(wrapper.find('.home_page')).to.have.length(1)
+  })
+})
